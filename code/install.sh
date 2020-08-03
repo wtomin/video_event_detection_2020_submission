5b757dd0af154e816cb4382c65c4a860116c9e5a
wtomin <dengdidanwtomin@gmail.com>
2020-08-03 16:54:17 +0800

    update install.sh

---
+++ code/install.sh
@@ -0,0 +1,6 @@
+#install
+pip install torch==1.3.0
+pip install numpy==1.16.0
+pip install argparse==1.1
+pip install tqdm==4.40.2
+pip install scikit-learn==0.22
---
+++ code/train_example.sh
@@ -0,0 +1,1 @@
+python train.py --store_name val_ratio_0.2_threshold_0.5_pos_weight_25.0_epochs_25 -b 128 -j 4 --epochs 25 --gpus 0
